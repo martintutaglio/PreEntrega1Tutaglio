ba8cfa44e06b906e5844b5cced4a4c05a3f31447
martin tutaglio <martintutaglio8@gmail.com>
2023-08-09 23:48:37 -0300

    ultima prueba

--- main.js
+++ main.js
@@ -1,20 +1,69 @@
 function calcularPrestamo() {
+  const { montoPrestamo, tasaInteresAnual, plazoAnos } = obtenerDatos();
+  const tasaMensual = calcularTasaMensual(tasaInteresAnual);
+  const numeroPagos = calcularNumeroPagos(plazoAnos);
+  const cuotaMensual = calcularCuotaMensual(montoPrestamo, tasaMensual, numeroPagos);
+  const montoTotal = calcularMontoTotal(cuotaMensual, numeroPagos);
+
+  mostrarResultado(cuotaMensual, montoTotal);
+}
+
+function obtenerDatos() {
   let montoPrestamo = parseInt(prompt("Ingrese el monto del préstamo:"));
   let tasaInteresAnual = parseInt(prompt("Ingrese la tasa de interés (%):"));
   let plazoAnos = parseInt(prompt("Ingrese el plazo (años):"));
 
-  const tasaMensual = tasaInteresAnual / 100 / 12;
-  const numeroPagos = plazoAnos * 12;
-  const cuotaMensual =
-    (montoPrestamo * tasaMensual) / (1 - Math.pow(1 + tasaMensual, -numeroPagos));
-  const montoTotal = cuotaMensual * numeroPagos;
+  return {
+    montoPrestamo,
+    tasaInteresAnual,
+    plazoAnos
+  };
+}
 
+function calcularTasaMensual(tasaInteresAnual) {
+  
+  return division(division(tasaInteresAnual, 100), 12);
+}
+
+function calcularNumeroPagos(plazoAnos) {
+  return multiplicacion(plazoAnos, 12);
+}
+
+function calcularCuotaMensual(montoPrestamo, tasaMensual, numeroPagos) {
+  return division(multiplicacion(montoPrestamo, tasaMensual), resta(1, potencia(suma(1, tasaMensual), -numeroPagos)));
+}
+
+function calcularMontoTotal(cuotaMensual, numeroPagos) {
+  return multiplicacion(cuotaMensual, numeroPagos);
+}
+
+function mostrarResultado(cuotaMensual, montoTotal) {
   let resultado =
     "Cuota Mensual: $" + cuotaMensual.toFixed(2) + "\nMonto Total a Pagar: $" + montoTotal.toFixed(2);
-
   alert(resultado);
 }
 
-calcularPrestamo();
+function suma(a, b) {
+  return a + b;
+}
 
-console.log
+function resta(a, b) {
+  return a - b;
+}
+
+function division(a, b) {
+  if (b === 0) {
+    throw new Error("No se puede dividir por cero.");
+  }
+  return a / b;
+}
+
+function multiplicacion(a, b) {
+  return a * b;
+}
+
+function potencia(base, exponente) {
+  return Math.pow(base, exponente);
+}
+
+calcularPrestamo();
